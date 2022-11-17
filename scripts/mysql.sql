be5d18237a19592a47e2444aec264d6b9bf8b9a1
Cliff Ingham <inghamn@bloomington.in.gov>
2022-11-17 13:47:35 -0500

    Code formatting cleanup

--- scripts/mysql.sql
+++ scripts/mysql.sql
@@ -1,27 +1,26 @@
--- @copyright 2015 City of Bloomington, Indiana
--- @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
--- @author Cliff Ingham <inghamn@bloomington.in.gov>
+-- @copyright 2015-2022 City of Bloomington, Indiana
+-- @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE
 create table people (
-	id int unsigned not null primary key auto_increment,
-	firstname varchar(128) not null,
-	lastname varchar(128) not null,
-	email varchar(255) not null,
-	username varchar(40) unique,
-	password varchar(40),
+	id                   int unsigned not null primary key auto_increment,
+	firstname            varchar(128) not null,
+	lastname             varchar(128) not null,
+	email                varchar(255) not null,
+	username             varchar(40) unique,
+	password             varchar(40),
 	authenticationMethod varchar(40),
-	role varchar(30)
+	role                 varchar(30)
 );
 
 create table aggregations (
-    id int unsigned not null primary key auto_increment,
-    name varchar(128) not null,
+    id                 int unsigned not null primary key auto_increment,
+    name               varchar(128) not null,
     google_calendar_id varchar(128) not null
 );
 
 create table aggregatedCalendars (
-    id int unsigned not null primary key auto_increment,
-    aggregation_id int unsigned not null,
-    name varchar(128) not null,
+    id                 int unsigned not null primary key auto_increment,
+    aggregation_id     int unsigned not null,
+    name               varchar(128) not null,
     google_calendar_id varchar(128) not null,
     foreign key (aggregation_id) references aggregations(id)
 );
